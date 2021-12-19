3a90239cbce0ee1105b7d781833e4a0f27ea6862
Alexey Dmitriev <aleks23041996@gmail.com>
2021-12-20 00:07:33 +0400

    Update Program.cs

--- WebApplication3Tanya/Program.cs
+++ WebApplication3Tanya/Program.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 namespace WebApplication3Tanya
 {
 	public class Program
-	{
+	
 		public static void Main(string[] args)
 		{
 			CreateHostBuilder(args).Build().Run();   
